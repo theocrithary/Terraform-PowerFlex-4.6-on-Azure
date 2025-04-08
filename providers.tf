8925bc9282f6a900168a74205e61bf337ec36973
theocrithary <theo@apacit.com.au>
2025-04-08 15:34:32 +1000

    .

--- providers.tf
+++ providers.tf
@@ -23,11 +23,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.14"
+      version = ">=4.14"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~>4.0"
+      version = ">=4.0"
     }
   }
 }
