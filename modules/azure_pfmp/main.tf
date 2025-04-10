4aaa5ee998153460ecde870d5565e33dce5239ae
theocrithary <theo@apacit.com.au>
2025-04-10 21:55:45 +1000

    .

--- modules/azure_pfmp/main.tf
+++ modules/azure_pfmp/main.tf
@@ -367,6 +367,12 @@ resource "azurerm_network_interface_backend_address_pool_association" "lb_be_poo
   backend_address_pool_id = azurerm_lb_backend_address_pool.lb_be_pool.id
 }
 
+data "azurerm_lb" "load_balancer" {
+  name                     = "${var.prefix}-lb"
+  resource_group_name      = local.resource_group.name
+}
+
+
 output "sds_nodes" {
   value = [
     for i in range(var.cluster.node_count) :
